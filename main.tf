dc05fb7ee33a3da13e4ad372017320c220c64713
Chris Geroux <chris.m.geroux@gmail.com>
2024-04-18 10:01:07 -0300

    Switch to larger avx2 only flavor, removing most GPU nodes

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ module "openstack" {
   instances = {
     mgmt   = { image="Rocky-9.3-x64-2023-11", type = "p8-12gb", tags = ["puppet", "mgmt", "nfs"], count = 1, disk_size=50 }
     login  = { image="Rocky-9.3-x64-2023-11", type = "p8-12gb", tags = ["login", "public", "proxy"], count = 1, disk_size=50 }
-    nodecpu   = { image="Rocky-9.3-x64-2023-11", type = "c4-15gb-83", tags = ["node"], count = 20 }
-    nodegpu   = { image="Rocky-8.9-x64-2023-11", type = "g1-8gb-c4-22gb", tags = ["node"], count = 20 }
+    nodecpu   = { image="Rocky-9.3-x64-2023-11", type = "c8-30gb-186-avx2", tags = ["node"], count = 10 }
+    nodegpu   = { image="Rocky-8.9-x64-2023-11", type = "g1-8gb-c4-22gb", tags = ["node"], count = 1 }
   }
 
   # var.pool is managed by Slurm through Terraform REST API.
